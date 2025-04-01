00ead0f36794d0ad03d666e3e2304902c47f0783
jarinjuan <r4s0vskyj4r0sl4v@gmail.com>
2025-04-01 20:37:50 +0200

    added the menu animation

--- zizkovskalajna/player.gd
+++ zizkovskalajna/player.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
-const SPEED: int = 90
+const SPEED: int = 200
 
 func _physics_process(delta: float) -> void:
 	var movement := Vector2.ZERO
 
 	if Input.is_action_pressed("move_right"):
-		movement.x += 1
+		movement.x += 2
 	if Input.is_action_pressed("move_left"):
-		movement.x -= 1
+		movement.x -= 2
 	if Input.is_action_pressed("move_down"):
-		movement.y += 1
+		movement.y += 2
 	if Input.is_action_pressed("move_up"):
-		movement.y -= 1
+		movement.y -= 2
 
 	if movement.length() > 0:
 		movement = movement.normalized()
