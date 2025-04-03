e434588a4bbcd0dd6143a37b963a05c23d1083ba
jarinjuan <r4s0vskyj4r0sl4v@gmail.com>
2025-04-03 21:40:09 +0200

    added main label movement in menu

--- zizkovskalajna/rich_text_label.gd
+++ zizkovskalajna/rich_text_label.gd
@@ -3,11 +3,17 @@ extends RichTextLabel
 var shake_amount = 0.3
 var colors = [Color.RED, Color.YELLOW, Color.PINK, Color.CYAN]
 
+var tilt_amount = 0.03	  #max uhel
+var tilt_speed = 2.0  #rychlost náklonu
+var tilt_time = 0.0 
+
+func _ready():
+	set_pivot_offset(size / 2)
+
 func _process(delta):
-	# Random třes
-	position.x += randf_range(-shake_amount, shake_amount)
-	position.y += randf_range(-shake_amount, shake_amount)
-	
-	# Změna barvy každou chvíli
-	if randi() % 10 == 0: 
-		text = "[color=" + colors.pick_random().to_html() + "]Zizkovska Lajna[/color]"
+	# todle meni barvicku toho textu, idk jestli to pouzijem
+	#if randi() % 10 == 0: 
+		#text = "[color=" + colors.pick_random().to_html() + "]Zizkovska Lajna[/color]"
+
+	tilt_time += delta * tilt_speed
+	rotation = sin(tilt_time) * tilt_amount
