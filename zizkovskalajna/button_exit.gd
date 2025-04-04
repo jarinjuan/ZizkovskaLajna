9d1f74c6182e953b5213846b7b5c7debfe3ab44b
MalkinCZ <jakubmalek07@gmail.com>
2025-04-04 21:31:22 +0200

    Add menu music for testing and Mute button

--- zizkovskalajna/button_exit.gd
+++ zizkovskalajna/button_exit.gd
@@ -1,5 +1,9 @@
 extends Button
 
 
-func _on_pressed() -> void:
+func _on_button_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_mute_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
