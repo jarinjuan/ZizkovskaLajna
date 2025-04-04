d4fc766580a12d0175e3538797eb159fdf4ed5e5
MalkinCZ <jakubmalek07@gmail.com>
2025-04-04 20:54:43 +0200

    Improve enemy movement

--- zizkovskalajna/character_body_2d.gd
+++ zizkovskalajna/character_body_2d.gd
@@ -1,11 +1,32 @@
 extends CharacterBody2D
 
-@onready var player = get_node("../Player")  
-const SPEED: int = 75 
+@onready var player = get_node("../Player")
+const SPEED: int = 100
+const RUN_AWAY_DISTANCE: float = 50.0  
+const DISTANCE_MARGIN: float = 5.0 
 
-func _process(delta: float) -> void:
-	var direction: Vector2 = (player.position - position).normalized() 
-	if direction.length() > 0:  
-		position += direction * SPEED * delta  
-		
-		
+var has_weapon: bool = false #pokud ma zbran tak bezi k hraci, pokud ne tak si udrzuje vzdalenost 70px od hrace a utika od/k nemu
+
+func _physics_process(delta: float) -> void:
+	var to_player: Vector2 = player.position - position
+	var distance_to_player: float = to_player.length()
+	var direction: Vector2 = Vector2.ZERO
+
+	if has_weapon:
+		direction = to_player.normalized()
+	else:
+		if distance_to_player < RUN_AWAY_DISTANCE - DISTANCE_MARGIN:
+			direction = (-to_player).normalized()
+		elif distance_to_player > RUN_AWAY_DISTANCE + DISTANCE_MARGIN:
+			direction = to_player.normalized()
+		else:
+			direction = Vector2.ZERO
+
+	if direction.length() < 0.01:
+		direction = Vector2.ZERO
+
+	var velocity = direction * SPEED
+
+	var collision = move_and_collide(velocity * delta)
+	if collision and collision.get_collider() == player:
+		velocity = Vector2.ZERO
