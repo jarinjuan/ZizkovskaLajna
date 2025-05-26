9acb6e961c027170e24cd86dfc43c87072485c64
MalkinCZ <jakubmalek07@gmail.com>
2025-05-26 19:46:18 +0200

    complete weapon system overhaul

--- zizkovskalajna/Player/player.gd
+++ zizkovskalajna/Player/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
-@export var fire_rate = 0.2;
+@export var fire_rate = 0.1;
 @export var melee_range: float = 50.0
+@export var bullets = 30
 var bullet_speed = 1000
 const SPEED: int = 200
-var current_weapon = null 
-var weapon_equipped = false
+@onready var weapon_socket = $WeaponSocket
+var current_weapon: Weapon = null
+var weapon_equipped := false
 var bullet = preload("res://Player/bullet.tscn")
 var can_fire = true
 
@@ -25,24 +27,18 @@ func _physics_process(delta: float) -> void: #movement
 	velocity = movement * SPEED
 	move_and_slide()
 
-func pick_up_weapon(weapon): #weapon_pickup
-	current_weapon = weapon.weapon_name
+func pick_up_weapon(weapon_scene: PackedScene) -> void:
+	if current_weapon:
+		current_weapon.queue_free()        # drop old gun
+	current_weapon = weapon_scene.instantiate() as Weapon
+	weapon_socket.add_child(current_weapon)
 	weapon_equipped = true
-	print("zvednuto: " + current_weapon) #kontrola jak to de
-	print(weapon_equipped)
 
 func _process(delta):
 	rotate(get_angle_to(get_global_mouse_position()))
 
-	if weapon_equipped and Input.is_action_pressed("fire") and can_fire:
-		var bullet_instance = bullet.instantiate()
-		bullet_instance.position = $BulletPoint.get_global_position()
-		var direction_to_mouse = (get_global_mouse_position() - bullet_instance.position).normalized()
-		bullet_instance.rotation = direction_to_mouse.angle()
-		get_tree().get_root().add_child(bullet_instance)
-		can_fire = false
-		await get_tree().create_timer(fire_rate).timeout
-		can_fire = true
+	if weapon_equipped and Input.is_action_pressed("fire") and current_weapon:
+		current_weapon.shoot(get_global_mouse_position())
 
 	# MELEE pokud nemám zbraň
 	elif not weapon_equipped and Input.is_action_just_pressed("fire"):
