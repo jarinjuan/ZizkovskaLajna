38910a51cf0bd3a0ad79364d5dc4381874f385cd
MalkinCZ <jakubmalek07@gmail.com>
2025-06-11 19:00:46 +0200

    Add navigationarea, implement porsche animation

--- zizkovskalajna/Scenes/Objects/Porsche/porsche.gd
+++ zizkovskalajna/Scenes/Objects/Porsche/porsche.gd
@@ -1,16 +1,23 @@
 extends Area2D
 
 @export var level_controller_path: NodePath
+
 var level_controller: Node
 signal level_finished
 
+var door_opened := false
+
 func _ready():
 	level_controller = get_node(level_controller_path)
+	$PorscheDoor.stop()
+	$PorscheDoor.frame = 0  
+	
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		
 		if level_controller.all_enemies_dead:
+			$PorscheDoor.play("openDoor")
+			await get_tree().create_timer(1.0).timeout
 			emit_signal("level_finished")
 		else:
 			print("Dodelej praci")
