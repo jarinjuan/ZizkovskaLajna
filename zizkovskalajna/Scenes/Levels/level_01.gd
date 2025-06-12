a70e88262a625f028b1995d61e1bd89e9a8985a9
jarinjuan <r4s0vskyj4r0sl4v@gmail.com>
2025-06-12 21:01:37 +0200

    Added dialogue for level 1 and level 2

--- zizkovskalajna/Scenes/Levels/level_01.gd
+++ zizkovskalajna/Scenes/Levels/level_01.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-var tutorial_message = "Dog."
-
-#func _process(delta: float) -> void:
-	#Ui.start_dialog(tutorial_message, "transmitter")
-	
+var brain_instructions = "Kill them. Kill them all."
+var tutorial_message = "Move using WASD. Turn around using your mouse. Shoot using left mouse button."
+var enemy_dead_sentence = "Peter, what the fuck are you doing?!"
+var x = 0
 
 func _ready():
 	await Ui.dialog_setup()
-	Ui.start_dialog(tutorial_message, "demo")
-
+	await Ui.start_dialog(tutorial_message, "transmitter")
+	await Ui.start_dialog(brain_instructions, "sprite_brain")
+	
 func _on_enemy_enemy_died() -> void:
-	Ui.start_dialog(tutorial_message, "demo")
+	while (x < 1):
+		Ui.start_dialog(enemy_dead_sentence, "transmitter")
+		x += 1
