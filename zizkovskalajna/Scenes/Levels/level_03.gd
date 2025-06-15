6ead7f2e715718fcadf885569d07ba0c0ec2f148
jarinjuan <r4s0vskyj4r0sl4v@gmail.com>
2025-06-15 18:07:16 +0200

    Added floor tilemap to level 2 and 3

--- zizkovskalajna/Scenes/Levels/level_03.gd
+++ zizkovskalajna/Scenes/Levels/level_03.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var brain_instructions = "We are not human anymore. We are simply a democracy tool."
 var player_response = "There has to be other option than to just kill everybody. I haven't slept for three days straight. I can't stop seeing their faces, even though I know they were bad people. We have to stop."
-var brain_instructions2 = "Stop? You *hesitate* now? After all the blood, after all the justice we've delivered? You knew the price. You knodddddddw what they did. Their screams mean nothing — they're echoes of a dying regime. And now you want mercy? Pathetic. Pick up the gun. Do what's necessary."
+var brain_instructions2 = "Stop? You *hesitate* now? After all the blood, after all the justice we've delivered? You knew the price. You know what they did. Their screams mean nothing — they're echoes of a dying regime. And now you want mercy? Pathetic. Pick up the gun. Do what's necessary."
 var x = 0
 
 func _ready():
